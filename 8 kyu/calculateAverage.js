04a5e1ba9451d022146672e4e8a01c232af742a4
Ryan Crespo <crespoffi@gmail.com>
2022-05-22 21:01:59 -0400

    daily kata practice

---
+++ 8 kyu/calculateAverage.js
@@ -0,0 +1,15 @@
+// CALCULATE AVERAGE
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+// SOLUTION: 
+
+function find_average(array) {
+    if (array.length === 0) {
+      return 0
+    } else {
+      return array.reduce((result, current) => result + current, 0) / array.length;
+    }
+  }
