949f2251a61a588912f5e4b9abce9026fe27b395
Ryan Crespo <crespoffi@gmail.com>
2022-06-08 23:09:02 -0400

    kata practice

---
+++ 8 kyu/reversedSequence.js
@@ -0,0 +1,15 @@
+// REVERSED SEQUENCE
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+// SOLUTION:
+
+function reverseSeq(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr.reverse();
+}
